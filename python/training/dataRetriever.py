a49e996e1e7b65bbd293a33a7ab56805ab09974e
Zaim Zazali <zaim.pe.srcutp@gmail.com>
2021-02-27 17:35:34 +0800

    Updated

--- python/training/dataRetriever.py
+++ python/training/dataRetriever.py
@@ -1,20 +1,42 @@
 import socket
+import threading
 from f1_2019_telemetry.packets import unpack_udp_packet
 
 class DataRetriever:
-    def __init__(self):
+    packet = None
+    toProceed = True
+
+    def getUDPdata(self):
+        return self.packet
+
+    def stopIterate(self):
+        self.toProceed = False
+
+    def startStreamData(self):
         try:
+            # Open and Connect to Socket
             udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
             udp_socket.bind(('', 20777))
-            udp_socket.settimeout(10) # To wait for 10 seconds
+            udp_socket.settimeout(5)
 
-            while True:
+            # Get the Data
+            while self.toProceed:
                 udp_packet = udp_socket.recv(2048)
-                packet = unpack_udp_packet(udp_packet)
-                print("Received:", packet)
-                print()
-                
-        except:
-            print("No data received!")
-            print()
-        
+                self.packet = unpack_udp_packet(udp_packet)
+                # print("Data received!")
+
+                if (not self.toProceed):
+                    break
+
+        except socket.timeout as e:
+            print('ERROR:', e)
+            self.stopIterate()
+
+    def __init__(self):
+        print("DataRetriever Instantiated!")
+
+        try:
+            th1 = threading.Thread(target=self.startStreamData)
+            th1.start()
+        except Exception as e:
+            print('Exception:', e, '\nERROR: Unable to start streaming thread!')
--- python/training/main.py
+++ python/training/main.py
@@ -1,9 +1,25 @@
+import threading
 from dataRetriever import DataRetriever
+from keyboardListener import KeyboardListener
 
 class main():
     def __init__(self):
-        _DataReceiver = DataRetriever
-        _DataReceiver()
+        _DataReceiver = DataRetriever()
+        _KeyboardListener = KeyboardListener()
+
+        toProceed = not _KeyboardListener.getToTerminate()
+        while toProceed:
+            toProceed = not _KeyboardListener.getToTerminate()
+            if toProceed:
+                data = _DataReceiver.getUDPdata()
+                try:
+                    print(data) 
+                except:
+                    pass
+            else:
+                _DataReceiver.stopIterate()
+
+        
 
 if __name__ == "__main__":
     main()
